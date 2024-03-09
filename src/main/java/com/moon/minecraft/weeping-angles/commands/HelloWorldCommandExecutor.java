a97740fda779f5ecdb4c5b5ee182a660cc53640c
Mosikoop <162281907+Mosikoop@users.noreply.github.com>
2024-03-09 18:59:35 +0530

    Create HelloWorldCommandExecutor.java

---
+++ src/main/java/com/moon/minecraft/weeping-angles/commands/HelloWorldCommandExecutor.java
@@ -0,0 +1,19 @@
+package com.moon.minecraft.commands;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.annotation.command.*;
+
+@Commands(@Command(name = "HelloWorldCommand", aliases = "helloworld", usage = "/hello"))
+public class HelloWorldCommandExecutor implements CommandExecutor {
+
+	public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+
+		if (sender instanceof Player)
+			sender.sendMessage("Hello " + sender.getName() + " to Minecraft!");
+		else
+			sender.sendMessage("Hello Minecraft!");
+		return true;
+	}
+}
