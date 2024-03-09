3865f798cab7f9e97b38feca358703ed4d4bf55d
Mosikoop <162281907+Mosikoop@users.noreply.github.com>
2024-03-09 18:54:54 +0530

    Update AppTest.java

--- src/test/java/com/moon/minecraft/weeping-angles/AppTest.java
+++ src/test/java/com/moon/minecraft/weeping-angles/AppTest.java
@@ -1,1 +1,9 @@
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("This is a test class for the application.");
+    }
 
+    public void testMethod() {
+        // Add your test logic here
+    }
+}
